baa3b30a08a699f327f636e9e248770a12ba87b2
harrison-symes <zanelockett@gmail.com>
2018-08-30 18:23:09 +1200

    added data file , deply done

---
+++ data/data.js
@@ -0,0 +1,27 @@
+const stories = {
+  story:[
+    {
+      chapter: 1,
+      name: 'Introduction story',
+      content: `I'm the stories content`,
+      image: './images/img.png',
+      sound: './sounds/sound1.wav'
+    },
+    {
+      chapter: 2,
+      name: 'Chapter two',
+      content: `I'm the stories content`,
+      image: './images/img.png',
+      sound: './sounds/soundtwo.wav'
+    },
+    {
+      chapter: 3,
+      name: 'Chapter three',
+      content: `I'm the stories content`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    }
+  ]
+}
+
+export default story
