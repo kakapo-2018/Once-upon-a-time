1c80ff5f961344bbf1217d4bdb0695b55c448c3c
EmilSchroder <eschro66@gmail.com>
2018-08-30 22:13:08 +1200

    chapters 1-9 content added

--- data/data.js
+++ data/data.js
@@ -2,25 +2,68 @@ const stories = {
   story:[
     {
       chapter: 1,
-      name: 'Introduction story',
-      content: `I'm the stories content`,
+      name: 'Chapter one',
+      content: `I'm only a baby and have just turned one, but I better start coding for a future of fun!`,
       image: './images/img.png',
       sound: './sounds/sound1.wav'
     },
     {
       chapter: 2,
       name: 'Chapter two',
-      content: `I'm the stories content`,
+      content: `I can't count to ten but I already know, there are only two numbers; one and zero.`,
       image: './images/img.png',
       sound: './sounds/soundtwo.wav'
     },
     {
       chapter: 3,
       name: 'Chapter three',
-      content: `I'm the stories content`,
+      content: `I tried self directed but I can't even read. My fingers are too pudgy, how will I succeed?`,
+      image: './images/img.png',
+      sound: './sounds/soundtwo.wav'
+    },
+    {
+      chapter: 4,
+      name: 'Chapter four',
+      content: `I looked high and low, threw my toys out the cot, all I wanted to create was a DDOSing bot.`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    },
+    {
+      chapter: 5,
+      name: 'Chapter five',
+      content: `World domination is a small ask, but still it is out of my tiny grasp.`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    },
+    {
+      chapter: 6,
+      name: 'Chapter six',
+      content: `At last I stumbled onto EDA, my demonic desires would at last find their way.`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    },
+    {
+      chapter: 7,
+      name: 'Chapter seven',
+      content: `But over days and weeks I began to turn. I no longer wanted to watch the world burn.`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    },
+    {
+      chapter: 8,
+      name: 'Chapter eight',
+      content: `I began to notice beauty in small things, like sharing a problem or template strings.`,
+      image: './images/img.png',
+      sound: './sounds/soundthree.wav'
+    },
+    {
+      chapter: 9,
+      name: 'Chapter nine',
+      content: `Now I have friends and skills I don't lust for riot, I can mindfully rest in peace and quiet.`,
       image: './images/img.png',
       sound: './sounds/soundthree.wav'
     }
+    
   ]
 }
 
