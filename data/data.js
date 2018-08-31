1ee550a29c63630655b57d001ca9dbe5448b4c07
student <leslie.alldridge>
2018-08-31 14:22:26 +1200

    added styles

--- data/data.js
+++ data/data.js
@@ -4,6 +4,7 @@ const stories = {
       chapter: 0,
       name: 'Introduction',
       content: `Baby's first Bootcamp`,
+      subtitle: 'I can\'t wait to get started!',
       image: './images/intro-baby.jpg',
       sound: './sounds/sound1.wav'
     }],
@@ -12,6 +13,7 @@ const stories = {
     chapter: 1,
     name: 'Chapter one',
     content: `I'm only a baby and have just turned one, but I better start coding for a future of fun!`,
+    subtitle: 'Learning html..',
     image: './images/1-coding-baby.jpg',
     sound: './sounds/sound1.wav'
   }],
@@ -21,6 +23,7 @@ const stories = {
       chapter: 2,
       name: 'Chapter two',
       content: `I can't count to ten but I already know, there are only two numbers; one and zero.`,
+      subtitle: 'Binary is life..',
       image: './images/2-binary-baby.jpg',
       sound: './sounds/soundtwo.wav'
     }
@@ -30,6 +33,7 @@ const stories = {
       chapter: 3,
       name: 'Chapter three',
       content: `I tried self directed but I can't even read. My fingers are too pudgy, how will I succeed?`,
+      subtitle: 'Fat fingering',
       image: './images/3-pudgy-fingers.jpg',
       sound: './sounds/soundtwo.wav'
     }],
@@ -38,6 +42,7 @@ const stories = {
       chapter: 4,
       name: 'Chapter four',
       content: `I looked high and low, threw my toys out the cot, all I wanted to create was a DDOSing bot.`,
+      subtitle: 'I\'m in denial',
       image: './images/4-toys-out-the-cot.jpg',
       sound: './sounds/soundthree.wav'
     }],
@@ -46,6 +51,7 @@ const stories = {
         chapter: 5,
         name: 'Chapter five',
         content: `World domination is a small ask, but still it is out of my tiny grasp.`,
+        subtitle: 'I can\'t wait to get started!',
         image: './images/5-fire-breathing-baby.jpg',
         sound: './sounds/soundthree.wav'
       }
@@ -55,6 +61,7 @@ const stories = {
       chapter: 6,
       name: 'Chapter six',
       content: `At last I stumbled onto EDA, my demonic desires would at last find their way.`,
+      subtitle: 'Beauty sleep is on my ToDo List..',
       image: './images/6-demon-baby.jpg',
       sound: './sounds/soundthree.wav'
     }],
@@ -63,6 +70,7 @@ const stories = {
       chapter: 7,
       name: 'Chapter seven',
       content: `But over days and weeks I began to turn. I no longer wanted to watch the world burn.`,
+      subtitle: '#peopleskills',
       image: './images/7-world-heat-map.jpg',
       sound: './sounds/soundthree.wav'
     }],
@@ -71,6 +79,7 @@ const stories = {
       chapter: 8,
       name: 'Chapter eight',
       content: `I began to notice beauty in small things, like sharing a problem or template strings.`,
+      subtitle: 'My name\'s Emil, Hello.',
       image: './images/8-template-strings.png',
       sound: './sounds/soundthree.wav'
     }],
@@ -79,6 +88,7 @@ const stories = {
       chapter: 9,
       name: 'Chapter nine',
       content: `Now I have friends and skills I don't lust for riot, I can mindfully rest in peace and quiet.`,
+      subtitle: 'The ice cubes melted..trust me..',
       image: './images/9-mindfulness-child.jpg',
       sound: './sounds/soundthree.wav'
     }
