d93c7960f1114067b2e6c86b7a56fa5398ee99d5
harrison-symes <zanelockett@gmail.com>
2018-08-31 14:38:14 +1200

    sounds with clicks

--- data/data.js
+++ data/data.js
@@ -11,76 +11,76 @@ const stories = {
   1: [ {
     chapter: 1,
     name: 'Chapter one',
-    content: `I'm only a baby and have just turned one, but I better start coding for a future of fun!`,
+    content: [`I'm only a `,'baby',' and have just turned one, but I better start coding for a future of fun!'],
     image: './images/1-coding-baby.jpg',
-    sound: './sounds/sound1.wav'
+    sound: './sounds/baby-cry.mp3'
   }],
 
     2: [
     {
       chapter: 2,
       name: 'Chapter two',
-      content: `I can't count to ten but I already know, there are only two numbers; one and zero.`,
+      content: [`I can't count to ten but I already know, there are only two numbers; `,`one and zero`,`.`],
       image: './images/2-binary-baby.jpg',
-      sound: './sounds/soundtwo.wav'
+      sound: './sounds/rtwodtwo.mp3'
     }
     ],
     3: [
     {
       chapter: 3,
       name: 'Chapter three',
-      content: `I tried self directed but I can't even read. My fingers are too pudgy, how will I succeed?`,
+      content: [`I tried self directed but I can't even read. My fingers are too `,`pudgy`,`, how will I succeed?`],
       image: './images/3-pudgy-fingers.jpg',
-      sound: './sounds/soundtwo.wav'
+      sound: './sounds/awww.mp3'
     }],
     4: [
     {
       chapter: 4,
       name: 'Chapter four',
-      content: `I looked high and low, threw my toys out the cot, all I wanted to create was a DDOSing bot.`,
+      content: [`I looked high and low, threw my toys out the cot, all I wanted to create was a`,` DDoSing bot`,`.`],
       image: './images/4-toys-out-the-cot.jpg',
-      sound: './sounds/soundthree.wav'
+      sound: './sounds/destroy.mp3'
     }],
     5: [
       {
         chapter: 5,
         name: 'Chapter five',
-        content: `World domination is a small ask, but still it is out of my tiny grasp.`,
+        content: [`World `,`domination `,`is a small ask, but still it is out of my tiny grasp.`],
         image: './images/5-fire-breathing-baby.jpg',
-        sound: './sounds/soundthree.wav'
+        sound: './sounds/lion.mp3'
       }
     ],
     6: [
     {
       chapter: 6,
       name: 'Chapter six',
-      content: `At last I stumbled onto EDA, my demonic desires would at last find their way.`,
+      content: [`At last I stumbled onto EDA, my `,`demonic `, `desires would at last find their way.`],
       image: './images/6-demon-baby.jpg',
-      sound: './sounds/soundthree.wav'
+      sound: './sounds/demongirl.mp3'
     }],
     7: [
     {
       chapter: 7,
       name: 'Chapter seven',
-      content: `But over days and weeks I began to turn. I no longer wanted to watch the world burn.`,
+      content: [`But over days and weeks I began to turn. I no longer wanted to watch the world `, `burn`,`.`],
       image: './images/7-world-heat-map.jpg',
-      sound: './sounds/soundthree.wav'
+      sound: './sounds/flame-arrow.mp3'
     }],
     8:[
     {
       chapter: 8,
       name: 'Chapter eight',
-      content: `I began to notice beauty in small things, like sharing a problem or template strings.`,
+      content: [`I began to notice `, `beauty `, `in small things, like sharing a problem or template strings.`],
       image: './images/8-template-strings.png',
-      sound: './sounds/soundthree.wav'
+      sound: './sounds/cartoon-birds.mp3'
     }],
     9:[
     {
       chapter: 9,
       name: 'Chapter nine',
-      content: `Now I have friends and skills I don't lust for riot, I can mindfully rest in peace and quiet.`,
+      content: [`Now I have friends and skills I don't lust for riot, I can mindfully rest in peace and `, `quiet`,`.`],
       image: './images/9-mindfulness-child.jpg',
-      sound: './sounds/soundthree.wav'
+      sound: './sounds/summer-crickets.mp3'
     }
   
   ]
